32f1786d6deb6121ebd1065d62bae5eee1dc2dde
Ismayil Shahaliyev <ishahaliyev@ada.local>
2024-06-24 10:53:43 +0400

    practical work 3

--- 03_camera/app.js
+++ 03_camera/app.js
@@ -14,7 +14,8 @@ onload = () => {
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
     gl.clearColor(0, 0, 1, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
 
     let vertices = [
         -0.5, -0.5, 0.5,
@@ -43,23 +44,23 @@ onload = () => {
     ];
 
     let colors = [
-        0, 0, 0, 1,
-        0, 0, 1, 1, 
-        0, 1, 0, 1,
-        0, 1, 1, 1,
-        1, 0, 0, 1,
-        1, 0, 1, 1,
-        1, 1, 0, 1,
-        1, 1, 1, 1,
+        0, 0, 0,
+        0, 0, 1,
+        0, 1, 0,
+        0, 1, 1,
+        1, 0, 0,
+        1, 0, 1,
+        1, 1, 0,
+        1, 1, 1,
     ];
-
-    let vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
     let iBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
+
+    let vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
     let vPosition = gl.getAttribLocation(program, 'vPosition');
     gl.vertexAttribPointer(vPosition,3,gl.FLOAT,gl.FALSE,0,0,);
@@ -70,8 +71,19 @@ onload = () => {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
 
     let vColor = gl.getAttribLocation(program, 'vColor');
-    gl.vertexAttribPointer(vColor,4,gl.FLOAT,gl.FALSE,0,0,);
+    gl.vertexAttribPointer(vColor,3,gl.FLOAT,gl.FALSE,0,0,);
     gl.enableVertexAttribArray(vColor);
+
+    let eye = [0, 0, 0];
+    let at = [1, 1, 1];
+    let up = [0.5, 0.5, 0];
+
+    let ctm = lookAt(eye, at, up);
+    // ctm = mult(ctm, translate(-0.2, 0, 0));
+    // ctm = mult(ctm, rotateZ(45));
+
+    let mvm = gl.getUniformLocation(program, 'mvm');
+    gl.uniformMatrix4fv(mvm, gl.FALSE, flatten(ctm));
 
     gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_BYTE, 0);
 }
